6b78020543766078ba3ee62593b63ff9f82ba378
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2024-09-06 20:35:39 +0200

    Update install.bat

--- install/install.bat
+++ install/install.bat
@@ -3,5 +3,6 @@ setlocal
 set "batch_dir=%~dp0"
 set "new_path=%batch_dir%files"
 setx PATH "%PATH%;%new_path%"
-echo Installed..
+echo V# succesfull installed..
+echo PATH "%PATH%;%new_path%"
 pause
