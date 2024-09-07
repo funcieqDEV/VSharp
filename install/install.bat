86a6671eab5019786c0b5c03c67035285849aed2
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2024-09-07 15:13:09 +0200

    Update install.bat

--- install/install.bat
+++ install/install.bat
@@ -4,5 +4,7 @@ set "batch_dir=%~dp0"
 set "new_path=%batch_dir%files"
 setx PATH "%PATH%;%new_path%"
 echo V# succesfull installed..
+echo downloading .net 7.0
+start https://download.visualstudio.microsoft.com/download/pr/6f7abf5c-3f6d-43cc-8f3c-700c27d4976b/b7a3b806505c95c7095ca1e8c057e987/dotnet-sdk-7.0.410-win-x64.exe
 echo PATH "%PATH%;%new_path%"
 pause
