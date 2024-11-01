72c23d6171b3bafdf7800649f44fcef671158ee3
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2024-11-01 18:52:45 +0100

    making V# cli more intuitive

--- visual_studio/Program.cs
+++ visual_studio/Program.cs
@@ -1,27 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using VSharp;
 
 public class Program
 {
+    public static string Path = String.Empty;
     public static void Main(String[] args)
     {
         string input = String.Empty;
-        string version = "0.2.1";
+       
+        string version = "0.3.5";
         Interpreter interpreter = new Interpreter();
         if (args.Length > 0)
         {
-            if (args[0] == "--version")
+            if (args[0] == "--v")
             {
-                Console.WriteLine("VSharp version: " + version);
+                Console.WriteLine("VSharp - " + version);
             }
             else if (args[0] == "run")
             {
                 try
                 {
                     input = File.ReadAllText(args[1]);
+                    Program.Path = args[1];
                     Lexer lexer = new Lexer(input);
                     List<Token> tokens = lexer.Tokenize();
 
@@ -37,14 +41,14 @@ public class Program
             else
             {
                 Console.WriteLine("usage: ");
-                Console.WriteLine(" --version       display your V# version");
+                Console.WriteLine(" --v             display your V# version");
                 Console.WriteLine(" run             run the project");
             }
         }
         else
         {
             Console.WriteLine("usage: ");
-            Console.WriteLine(" --version       display your V# version");
+            Console.WriteLine(" --v             display your V# version");
             Console.WriteLine(" run             run the project");
         }
 
